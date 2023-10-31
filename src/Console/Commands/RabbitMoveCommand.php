df0dc04857cbffced5ed78f2c1a0732b78693c0a
Temur5319436 <temur5319436@gmail.com>
2023-10-31 18:59:15 +0500

    refactor: closing channel and connection after moving

--- src/Console/Commands/RabbitMoveCommand.php
+++ src/Console/Commands/RabbitMoveCommand.php
@@ -4,18 +4,14 @@ namespace App\Rabbitmq\Console\Commands;
 
 use App\Rabbitmq\Rabbit\Client;
 use Illuminate\Console\Command;
-use PhpAmqpLib\Channel\AMQPChannel;
 
 class RabbitMoveCommand extends Command
 {
-    private AMQPChannel $channel;
     public function __construct(
         private readonly Client $client,
     )
     {
         parent::__construct();
-
-        $this->channel = $this->client->getChannel();
     }
 
     /**
@@ -46,7 +42,9 @@ class RabbitMoveCommand extends Command
             'effected' => 0,
         ];
 
-        while ($message = $this->channel->basic_get($from)) {
+        $channel = $this->client->getChannel();
+
+        while ($message = $channel->basic_get($from)) {
             $info['total']++;
 
             $data = $this->client->unserialize($message->getBody());
@@ -61,6 +59,9 @@ class RabbitMoveCommand extends Command
             $message->ack();
             $info['effected']++;
         }
+
+        $this->client->getChannel()->close();
+        $this->client->getConnection()->close();
 
         $this->info(
             'Messages are moved!' . chr(10) .
